ff146c5e225676717a4e629058a12348c3cbbb34
alisher <alisher@gmail.com>
2024-09-28 19:14:51 +0500

    oy_4_dars_4

---
+++ js/index.js
@@ -0,0 +1,98 @@
+// alert(
+//   "Assalomu Alaykum. xurmatli foydalanuvchi siz 1-nchi o'rinda console ni oching"
+// );
+
+// 1 - masala;
+
+// let num = +prompt("Sonni kiriting...");
+// if (num > 0) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Manfiy son");
+// }
+
+// 2 - masala;
+
+// let num = +prompt("Sonni kiriting ...");
+// if (num % 2 == 0) {
+//   console.log("Juft son");
+// } else if (num % 2 == 1) {
+//   console.log("Toq son");
+// }
+
+// 3 - masala;
+
+// let son1 = +prompt("1-soni kiriting ...");
+// let son2 = +prompt("2-sonni kiriting ...");
+
+// if (son1 > son2) {
+//   console.log("Katta");
+// } else if (son1 < son2) {
+//   console.log("Kichik");
+// } else {
+//   console.log("Teng");
+// }
+
+// 4 - masala;
+
+// let uzun = +prompt("Uzunlikni kiriting ...");
+// let eni = +prompt("Enini kiriting ...");
+
+// if (uzun == eni) {
+//   console.log("Kvadrat");
+// } else {
+//   console.log("Kvadrat emas");
+// }
+
+// 5 - masala;
+
+// let harorat = +prompt("Haroratni kiriting ...");
+
+// if (harorat > 0) {
+//   console.log("Yozgi");
+// } else {
+//   console.log("Qishgi");
+// }
+
+// 6 - masala;
+
+// let baho = +prompt("O'quvchinig bahosini kiriting ...");
+
+// if (baho >= 5) {
+//   console.log("A'lo");
+// } else if (baho >= 4) {
+//   console.log("Yaxshi");
+// } else if (baho >= 3) {
+//   console.log("Qoniqarli");
+// } else if (baho >= 2) {
+//   console.log("Yomon");
+// } else if (baho >= 1) {
+//   console.log("Dadangni telefon raqmini ber");
+// }
+
+//7 - masala;
+
+// let son1 = +prompt("1-sonni kiriting ...");
+// let son2 = +prompt("2-sonni kiriting ...");
+// let son3 = +prompt("3-sonni kiriting ...");
+
+// let max;
+// if (son1 > son2) {
+//   son1 = max;
+// } else {
+//   max = son2;
+// }
+
+// if (max < son3) {
+//   max = son3;
+// }
+// console.log(max);
+
+// 8 - masala;
+
+let son = prompt("Sonni kiriting...");
+if ((son = 5)) {
+  console.log("Besh xonaliy");
+} else {
+  console.log("Besh xonali emas");
+}
